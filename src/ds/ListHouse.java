9bd3f94347d0f929fd54595a6f171ad84a54626f
Hasitha Kalindu <hasithakalindu@gmail.com>
2015-04-03 12:00:24 +0530

    Second Commit. Created ListHouse class and declared essential variables.

---
+++ src/ds/ListHouse.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ds;
+
+/**
+ *
+ * @author TS
+ */
+public class ListHouse {
+     // House Information
+    private int lotNumber;
+    private int squareFeet;
+    private int bedRooms;
+    private int price;
+    
+    private String lastName;
+    private String firstName;
+
+    public ListHouse(int lotNumber, int squareFeet, int bedRooms, int price, String lastName, String firstName) {
+        this.lotNumber = lotNumber;
+        this.squareFeet = squareFeet;
+        this.bedRooms = bedRooms;
+        this.price = price;
+        this.lastName = lastName;
+        this.firstName = firstName;
+    }
+   
+}
